fe4fc34759463623cd9ed27a8be7c92081ee2c61
TheGenomeWhisperer <topping@email.arizona.edu>
2015-04-14 12:16:24 -0700

    Establishing the first file with lead comment.

---
+++ QuestingHelps.cs
@@ -0,0 +1,9 @@
+/* Author: 	Sklug a.k.a TheGenomeWhisperer
+|   		The following functions are commonly called to be used as a "help"
+|		    For common scripting events as part of questing profile behaviors.
+| NOTE: 	"ExecuteLua" API function executes "LUA" code language inserted into the C#
+| NOTE: 	ALSO!!! This not a more standardized API with setters and getters, which ultimately would be nice,
+| NOTE:		but I am writing more focused script functions specifically for questing, so 
+| NOTE:		please understand if this is lacking common programming practices :D
+|
+*/
