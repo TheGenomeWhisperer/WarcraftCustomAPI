cc57229d1923fd49492ab3134d7dacda0f1cc00b
TheGenomeWhisperer <topping@email.arizona.edu>
2015-04-21 13:18:54 -0700

    Added new Zone Ability method. cleaned comments

--- QuestingHelps.cs
+++ QuestingHelps.cs
@@ -1,3 +1,4 @@
+
 /* Author:   	Sklug a.k.a TheGenomeWhisperer
 |       	  	The following functions are commonly called to be used as a "help"
 |		        For common scripting events as part of questing profile behaviors.
@@ -6,31 +7,91 @@
 | NOTE:	    	but I am writing more focused script functions specifically for questing, so 
 | NOTE:	    	please understand if this is lacking common programming practices :D
 | Final Note:   Class does not need Static Main as it will be injected into the Rebot.exe through the "Editor"
-|              Additional Information can be found at Rebot.to
+|               Additional Information can be found at Rebot.to
 */  
 
-
-class QuestingHelps
+public class QuestingHelps
 {
 
     public static ReBotAPI API;
     public static Fiber<int> QH;
 
     public QuestingHelps() { }
-    // Function:      GorgrondGarrisonAbility()
-    // What it does:  Moves to Focus Position, then uses zone ability 3 times on target location.
-    // Purpose:       To Counteract the Combat Program, as once it begins, it freezes all script functions
-    //	              as priority until combat stops, I disable combat ability until after function as completed.
+
+
+    // What it does:  It returns how many quests are remaining to complete within the profile.
+    // Purpose:       Basically to just keep track of profile progress so the player can know how much is remaining
+    //                to Do without having to dig through this.
+    public static int RemainingQuests(int[] questArray)
+    {
+        int count = 0;
+
+        for (int i = 0; i < questArray.Length; i++)
+        {
+            if (!API.IsQuestCompleted(questArray[i]))
+            {
+                count++;
+            }
+        }
+        API.Print("You Have " + count + " quests left to complete in this questpack!");
+        return count;
+    }
+
+
+    // What it does:  Moves to Focus Position, then uses zone ability, prioritizing both attacks.
+    // Purpose:       Using the zone ability is critical, and to keep the code from getting too 
+    //                bloated, calling this method should be a more sufficient method for repeat use.
     public static IEnumerable<int> GorgrondGarrisonAbility()
     {
         API.DisableCombat = true;
+        while (API.Me.Focus.Distance2D > 10.0)
+        {
 
-        while (API.Me.Focus.Distance2D > 5)  // This ensure player paths to the focus target until it is 5 yrds or closer.
+            API.CTM(API.Me.Focus.Position);
+            yield return 200;
+        }
+        API.ExecuteMacro("/use Garrison Ability");
+        yield return 3000;
+        while (API.Me.IsOnTransport && API.Me.Focus != null && !API.Me.Focus.IsDead)
+        {
+            API.SetFacing(API.Me.Focus);
+            if (API.Me.Focus.Distance2D <= 13)
+            {
+                API.ExecuteMacro("/click OverrideActionBarButton2");
+            }
+            else
+            {
+                while (API.Me.Focus.Distance2D > 13.0)
+                {
+                    API.CTM(API.Me.Focus.Position);
+                    yield return 100;
+                }
+                API.ExecuteMacro("/click OverrideActionBarButton2");
+            }
+            API.SetFacing(API.Me.Focus);
+            for (int i = 0; i < 3; i++)
+            {
+                API.ExecuteMacro("/click OverrideActionBarButton1");
+                Random rnd = new Random();
+                yield return rnd.Next(1700, 1900);
+            }
+        }
+        API.ExecuteMacro("/click OverrideActionBarLeaveFrameLeaveButton");
+        API.DisableCombat = false;
+    }
+
+    // What it does:  Moves to Focus Position, then uses zone ability 3 times.
+    // Purpose:       Due to the nature of having to call the Garrison ability multiple times when facing
+    //                challenging rare monsters in the world, it would be more efficient to
+    //                merely require just calling this method.
+    public static IEnumerable<int> TaladorGarrisonAbility()
+    {
+        API.DisableCombat = true;
+        while (API.Me.Focus != null && API.Me.Focus.Distance2D > 5)  // This ensure player paths to the focus target until it is 5 yrds or closer.
         {
             API.MoveTo(API.Me.Focus.Position);
             yield return 100;
         }
-
         for (int i = 0; i < 3; i++)
         {
             API.ExecuteMacro("/use Garrison Ability");
@@ -38,33 +99,10 @@ class QuestingHelps
             API.ClickOnTerrain(API.Me.Focus.Position);
             yield return 1500;
         }
-
         API.DisableCombat = false;
     }
-    
-    // Function:      PlayerNeedsExpPotions(int)
-    // What it does:  Returns a boolean on if it needs to purchase potions or not.
-    // Purpose:       To determine if the player has enough and can buy more XP potions.
-    //	              This is a good boolean gate to prevent the need to execute the bulkier
-    //                in the "BuyExpPotions(int)" method if player has no need.
-    public static bool PlayerNeedsExpPotions(int maxOwn)
-    {
-        bool result = false;
-        int currentPotionCount = API.ExecuteLua<int>("local potions = GetItemCount(120182); return potions;");
 
-        if (currentPotionCount < maxOwn)
-        {
-            int gResources = API.ExecuteLua<int>("local _, amount = GetCurrencyInfo(824); return amount;");
-            int canBuy = gResources / 100;
-            if (canBuy != 0)
-            {
-                result = true;
-            }
-        }
-        return result;
-    }
-    
-`   // Function:      BuyExpPotions(int)
+
     // What it does:  Algorithm that Determines Amount of XP potions in possession, if you need more, and if you havunds for more.
     //                then it purchases the indicated amount to buy for you from vendor.
     // Purpose:       To automate XP potion purchasing without wasting Garrison resources.  By automating the purchas of
@@ -111,8 +149,31 @@ class QuestingHelps
             API.Print("You Already Have a Sufficient Supply of XP Potions! YAY!");
         }
     }
-    
-    // Function:      UseGuildBanner()
+
+
+    // What it does:  Returns a boolean on if it needs to purchase potions or not.
+    // Purpose:       To determine if the player has enough and can buy more XP potions.
+    //		          This is a good boolean gate to prevent
+    //		  
+    public static bool PlayerNeedsExpPotions(int maxOwn)
+    {
+        bool result = false;
+        int currentPotionCount = API.ExecuteLua<int>("local potions = GetItemCount(120182); return potions;");
+
+        if (currentPotionCount < maxOwn)
+        {
+            int gResources = API.ExecuteLua<int>("local _, amount= GetCurrencyInfo(824); return amount;");
+            int canBuy = gResources / 100;
+            if (canBuy != 0)
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+
+
     // What it does:  Uses a Guild Banner at Player Position by, prioritization of best (15% bonus gains) to worse (5%).
     // Purpose:       If the banner is not on cooldown, it will use it when called upon so player may level faster.
     //	              Also, it prioritizes the best of the banners to be used so you are always using best one available.
@@ -134,17 +195,17 @@ class QuestingHelps
             API.UseItem(64400);
         }
     }
-    
-    // Function:      PlaceGuildBannerAtDestination(Vector3)
+
+
     // What it does:  Basic check to first, see if one of the 3 banners is available.  If so,
-    //		      it will execute the MoveTo to the given location and then use an available banner.
+    //		          it will execute the MoveTo to the given location and then use an available banner.
     // Purpose:       The reason why this is separated from the previous function GuildBanners() is because
-    //		      it is necessary to do a check before telling the player to moveTo a destination.
-    //		      It would be time-wasting of the player to move to the destination to use banner if it
-    //		      was not currently available in the first place.
-    //		      The MAJOR advantage using a Vector3 as an argument and not explicitly listing the argument is
-    //		      that I can call to this whenever I wish to, for whatever reason, and if I wish to pass it an
-    //		      explicit pre-determined location I can, or I can implement give it an object location as well.
+    //		          it is necessary to do a check before telling the player to moveTo a destination.
+    //		          It would be time-wasting of the player to move to the destination to use banner if it
+    //		          was not currently available in the first place.
+    //		          The MAJOR advantage using a Vector3 as an argument and not explicitly listing the argument is
+    //		          that I can call to this whenever I wish to, for whatever reason, and if I wish to pass it an
+    //		          explicit pre-determined location I can, or I can implement give it an object location as well.
     public static IEnumerable<int> PlaceGuildBannerAtDestination(Vector3 travel)
    {
         if ((API.HasItem(64402) && API.ItemCooldown(64402) == 0) || (API.HasItem(64401) && API.ItemCooldown(64401) == 0) || (API.HasItem(64400) && API.ItemCooldown(64400) == 0))
@@ -160,8 +221,8 @@ class QuestingHelps
 	    {
 		     API.Print("Guild Banner Not Currently Available");
 	    }
-    }   
-    
+    }
+
     /*
     |  The Following Banner method is based on the idea of having an aura check whilst in combat
     | and if you are lacking the aura (i.e. no banner nearby), then it drops one, rather than
@@ -169,19 +230,19 @@ class QuestingHelps
     |  from the main thread to run in parallel to the script to aura check in the background.
     */
 
-    // Function:      PlaceGuildBannerOnAuraCheck()
+
     // What it does:  Uses a Guild Banner at Player Position by, prioritization of best (15% bonus gains) to worse (5%).
     //                To Be Used recursively, hence it yield breaks if player hits lvl 99, or is player has no banners.
     //                This also delays the continuous check if all 3 banners are on CD, and it ONLY does a check
     //                to be used if in combat.
     // Purpose:       If the banner is not on cooldown, it will use it when called upon so player may level faster.
     //	              Also, it prioritizes the best of the banners to be used so you are always using best one available.
-    //		      What makes this different from previous UseGuildBanner() function is...
-    //		      ...in effort to conserve from spamming the conditionals, if all 3 banners are on cooldown
-    //		      then it determines the shortest cooldown time then "yield returns" or "waits" to carry on
-    //		      Ideally, this is a feature to be used with a background aura that checks continuously.
-    //		      WARNING! DO NOT USE unless thread is forked and this is checking in the background in parallel
-    //		      If you use it in main thread, bot will not continue whilst banners on cooldown.
+    //		          What makes this different from previous UseGuildBanner() function is...
+    //		          ...in effort to conserve from spamming the conditionals, if all 3 banners are on cooldown
+    //		          then it determines the shortest cooldown time then "yield returns" or "waits" to carry on
+    //		          Ideally, this is a feature to be used with a background aura that checks continuously.
+    //		          WARNING! DO NOT USE unless thread is forked and this is checking in the background in parallel
+    //		          If you use it in main thread, bot will not continue whilst banners on cooldown.
     public static IEnumerable<int> PlaceGuildBannerOnAuraCheck()
     {
         if (!API.HasItem(64402) && !API.HasItem(64401) && !API.HasItem(64400))
@@ -222,6 +283,7 @@ class QuestingHelps
                 {
                     waitTime = Banner3;
                 }
+                // Determining Cooldown remaining on next banner.
                 int delay = (int)waitTime;
                 delay = delay * 1000;
                 int minutes = delay / 60000;
@@ -235,27 +297,8 @@ class QuestingHelps
         yield return wait;
         PlaceGuildBannerOnAuraCheck();
     }
-    
-    // Function:      RemainingQuests(int[])
-    // What it does:  It returns how many quests are remaining to complete within the profile.
-    // Purpose:       Basically to just keep track of profile progress so the player can know how much is remaining
-    //                to Do each time player begins the script
-    public static int RemainingQuests(int[] questArray)
-    {
-        int count = 0;
 
-        for (int i = 0; i < questArray.Length; i++)
-        {
-            if (!API.IsQuestCompleted(questArray[i]))
-            {
-                count++;
-            }
-        }
-        API.Print("You Have " + count + " quests left to complete in this questpack!");
-        return count;
-    }
 
-    // Function:      questObjectiveProgress(int, int, string)
     // What it does:  This does a boolean check on quest progress for a given objective and
     //                returns true if the current quest progress matches the one given in the argument.
     // Purpose:       Occasionally when writing quest templates, sometimes you want something to keep
@@ -280,7 +323,5 @@ class QuestingHelps
         }
         return result;
     }
-    
-    
-// End Class
+
 }
