8fb2fd1f937dda51a0bec44e50b406ef40cc72d7
TheGenomeWhisperer <topping@email.arizona.edu>
2015-10-08 02:16:58 -0700

    Added new C# Script to work on... FlightManager.cs

---
+++ FlightManager.cs
@@ -0,0 +1,13 @@
+// TravelManager
+
+
+
+
+public class FlightManager
+{
+    public static ReBotAPI API;
+    public static Fiber<int> Fib;
+
+    // Empty Constructor
+    public FlightManager() { }	
+		
